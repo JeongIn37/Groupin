b8d99f53e67801c51dd17335b9c67bd5a2010c42
yoonjeongin <yoonjeongin@naver.com>
2022-07-14 14:57:24 +0900

    initial commit

---
+++ src/main/java/com/songpyeon/groupin/GroupinApplication.java
@@ -0,0 +1,13 @@
+package com.songpyeon.groupin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/songpyeon/groupin/GroupinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songpyeon.groupin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
