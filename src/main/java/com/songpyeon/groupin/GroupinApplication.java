aeed38139db4fb9ba6fc9b4445487f7f0c6f8885
yoonjeongin <yoonjeongin@naver.com>
2022-07-23 20:51:56 +0900

    [SETTING] Enable auto login page in SecurityAutoConfiguration
    WebSecurityConfigurerAdapter 지원 종료로 main java 파일에서 기본 security setting enable

--- src/main/java/com/songpyeon/groupin/GroupinApplication.java
+++ src/main/java/com/songpyeon/groupin/GroupinApplication.java
@@ -2,8 +2,9 @@ package com.songpyeon.groupin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class GroupinApplication {
 
     public static void main(String[] args) {
